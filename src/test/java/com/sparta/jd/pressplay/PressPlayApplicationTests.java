d5e9a51fbd0176fdeb3057b31bb0cf1a6b57c4fc
JamesDorling <jamesrdorling@gmail.com>
2022-01-26 12:02:01 +0000

    initial commit

---
+++ src/test/java/com/sparta/jd/pressplay/PressPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jd.pressplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
